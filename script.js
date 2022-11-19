d0b6b9a5994c89e6f7f0f26e234c4bae6d81241c
daniel p d <danieldavis9312@gmail.com>
2022-11-19 05:31:03 +0530

    select HTML elements

---
+++ script.js
@@ -0,0 +1,8 @@
+'use strict';
+// select elements 
+const formEl = document.getElementById('form');
+const usernameEl = document.getElementById('username');
+const emailEl = document.getElementById('email');
+const mobileEl = document.getElementById('mobile');
+const newpasswordEl = document.getElementById('new-password');
+const confirmpasswordEl = document.getElementById('confirm-password');
