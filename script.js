3c2f034645cc254f21a4cafb5fe3908380196858
daniel p d <danieldavis9312@gmail.com>
2022-11-22 21:46:32 +0530

    input validation mandatory

--- script.js
+++ script.js
@@ -8,50 +8,73 @@ const newpasswordEl = document.getElementById('new-password');
 const confirmpasswordEl = document.getElementById('confirm-password');
 
 //functions 
+const inputElArrays =[usernameEl, emailEl, mobileEl,newpasswordEl, confirmpasswordEl];
+
+const checkRequriedInput = function(arr){
+  for(const el of arr){
+if(el.value ===''){
+  const formControlEl = el.parentElement;
+  formControlEl.classList = 'form-control error';
+}else{
+  const formControlEl=el.parentElement;
+  formControlEl.classList='form-control success';
+}
+}
+}
+formEl.addEventListener('submit',(e)=>{e.preventDefault();
+checkRequriedInput(inputElArrays);}
+);
 
 
 
 //events  
 //add event listener
-formEl.addEventListener('submit', (e)=>{e.preventDefault();
-if(usernameEl.value === ''){
-  const formControlEl = usernameEl.parentElement;
-  formControlEl.classList = 'form-control error';
-  }
-  else{
-    const formControlEl = usernameEl.parentElement;
-    formControlEl.classList = 'form-control success';
-  }
-  if(emailEl.value === ''){
-    const formControlEl = emailEl.parentElement;
-    formControlEl.classList= 'form-control error';
-    }
-    else{
-      const formControlEl = emailEl.parentElement;
-      formControlEl.classList ='form-control success';
-    }
-    if(mobileEl.value === ''){
-      const formControlEl = mobileEl.parentElement;
-      formControlEl.classList= 'form-control error';
-      }
-      else{
-        const formControlEl = mobileEl.parentElement;
-        formControlEl.classList ='form-control success';
-      }
-      if(newpasswordEl.value === ''){
-        const formControlEl = newpasswordEl.parentElement;
-        formControlEl.classList= 'form-control error';
-        }
-        else{
-          const formControlEl = newpasswordEl.parentElement;
-          formControlEl.classList ='form-control success';
-        }
-        if(confirmpasswordEl.value === ''){
-          const formControlEl = confirmpasswordEl.parentElement;
-          formControlEl.classList= 'form-control error';
-          }
-          else{
-            const formControlEl = confirmpasswordEl.parentElement;
-            formControlEl.classList ='form-control success';
-          }
-});
+
+
+
+// ******************************basic addEventListener code (with hydration  method  written below the function heading up  ************************* 
+
+
+
+// formEl.addEventListener('submit', (e)=>{e.preventDefault();
+// if(usernameEl.value === ''){
+//   const formControlEl = usernameEl.parentElement;
+//   formControlEl.classList = 'form-control error';
+//   }
+//   else{
+//     const formControlEl = usernameEl.parentElement;
+//     formControlEl.classList = 'form-control success';
+//   }
+//   if(emailEl.value === ''){
+//     const formControlEl = emailEl.parentElement;
+//     formControlEl.classList= 'form-control error';
+//     }
+//     else{
+//       const formControlEl = emailEl.parentElement;
+//       formControlEl.classList ='form-control success';
+//     }
+//     if(mobileEl.value === ''){
+//       const formControlEl = mobileEl.parentElement;
+//       formControlEl.classList= 'form-control error';
+//       }
+//       else{
+//         const formControlEl = mobileEl.parentElement;
+//         formControlEl.classList ='form-control success';
+//       }
+//       if(newpasswordEl.value === ''){
+//         const formControlEl = newpasswordEl.parentElement;
+//         formControlEl.classList= 'form-control error';
+//         }
+//         else{
+//           const formControlEl = newpasswordEl.parentElement;
+//           formControlEl.classList ='form-control success';
+//         }
+//         if(confirmpasswordEl.value === ''){
+//           const formControlEl = confirmpasswordEl.parentElement;
+//           formControlEl.classList= 'form-control error';
+//           }
+//           else{
+//             const formControlEl = confirmpasswordEl.parentElement;
+//             formControlEl.classList ='form-control success';
+//           }
+// });
